b7c17b81568ed1fb44758f7b9f95d189efac4802
skjalal99 <skjalal99ats@gmail.com>
2021-10-25 17:02:17 +0300

    admin-dashboard

--- admin/index.php
+++ admin/index.php
@@ -1,9 +1,25 @@
 <?php include('includes/header-admin.php');?>
 
 <?php include('includes/aside.php');?>
+<?php include_once('config/dbconn.php');?>
+<?php
+if(!isset($_SESSION['User-Login']))
+{
+	header("Location:login.php");
+}
+
+?>
+
 <!-- ======== main-wrapper start =========== -->
     <main class="main-wrapper">
-      
+    <?php
+    if(isset($_SESSION['Login']))
+    {
+        echo $_SESSION['Login'];
+        unset( $_SESSION['Login']);
+    }
+    
+    ?>
     <?php include('includes/navbar.php');?>
     <?php include('includes/main.php');?>
     <?php include('includes/footer1.php');?>
--- admin/login.php
+++ admin/login.php
@@ -1,12 +1,26 @@
 <?php include('../includes/header.php');?>
+<?php include_once('config/dbconn.php');?>
+
+<?php
 
 
-<style>
- 
+if(isset($_SESSION['Login'])) 
+{
+	echo $_SESSION['Login'];
+	unset ($_SESSION['Login']);
+}
+if(isset($_SESSION['Not-Login'])) 
+{
+	echo $_SESSION['Not-Login'];
+	unset ($_SESSION['Not-Login']);
+}
+if(isset($_GET['loggedout'])) 
+{
+	echo "<div class='alert alert-warning text-center' role='alert'>".$_GET['loggedout']."</div>";
 
-</style>
+}
 
-
+?>
 <div class="container">
 	<div class="d-flex justify-content-center h-100 login-wrap">
 		<div class="card">
@@ -20,20 +34,20 @@
 						<div class="input-group-prepend">
 							<span class="input-group-text input-group-text-login"><i class="fas fa-user"></i></span>
 						</div>
-						<input type="text" class="form-control" placeholder="username">
+						<input type="text" class="form-control" name="user_name" placeholder="username">
 						
 					</div>
 					<div class="input-group form-group form-group1">
 						<div class="input-group-prepend">
 							<span class="input-group-text input-group-text-login"><i class="fas fa-key"></i></span>
 						</div>
-						<input type="password" class="form-control" placeholder="password">
+						<input type="password" class="form-control" name="user_pass" placeholder="password">
 					</div>
 					<div class="row align-items-center remember">
 						<input type="checkbox">Remember Me
 					</div>
 					<div class="form-group">
-						<input type="submit" value="Login" class="btn float-end login_admin_btn">
+						<input type="submit" name="user_submit" value="Login" class="btn float-end login_admin_btn">
 					</div>
 				</form>
 			</div>
@@ -43,5 +57,37 @@
 </div>
 
 
+<?php
 
+if(isset($_POST['user_submit']))
+{
+	$username = $_POST['user_name'];
+	$pass = md5($_POST['user_pass']);
+	$sql = "SELECT * FROM USERS WHERE user_name ='$username' AND password ='$pass'";
+	
+	$res = $conn->query($sql);
+
+	if($res == TRUE)
+	{
+		$count = $res->num_rows;
+
+		if($count == 1)
+		{
+		$_SESSION['User-Login'] =$username;
+		echo "success";
+		$_SESSION['Login'] = "<div class='alert alert-success text-center' role='alert'>You are successfully logged in </div>";
+		header("Location:index.php");
+
+		}
+		else
+		{
+			$_SESSION['Login'] = "<div class='alert alert-warning text-center' role='alert'>Try Logging Again! </div>";
+			header("Location:login.php");
+		}
+
+	}
+
+}
+
+?> 
 
