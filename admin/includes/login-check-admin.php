aa593fbfdfe5069555972b50af72a4bafbf65971
skjalal99 <skjalal99ats@gmail.com>
2021-11-28 18:46:08 +0300

    admin-dashboard

---
+++ admin/includes/login-check-admin.php
@@ -0,0 +1,17 @@
+
+<?php
+if(!isset($_SESSION['User-Login']))
+{
+    $_SESSION['Not-Login'] = "<div class='alert alert-warning text-center' role='alert'>You're not logged in! </div>";
+		
+	    // Navigate to login page:
+        header("Location:../login.php");
+        echo "Not logged In";
+
+}
+else{
+    echo "==============>".$_SESSION['User-Login'];
+}
+?>
+
+
